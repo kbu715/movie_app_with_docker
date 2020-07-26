a8ea5c06f0ab1f2faf5270f48385558815a9f083
kbu715 <kbu715@gmail.com>
2020-07-26 20:33:10 +0900

    admin jinwoo

--- client/src/Routes/Detail/Cast/CastItem.js
+++ client/src/Routes/Detail/Cast/CastItem.js
@@ -15,11 +15,14 @@ const MovieImg = styled.img`
 
 const CastItem = ({ person }) => {
   return (
+    <>
     <MovieImg
       src={`${IMAGE_BASE_URL}w185${
         person.profile_path ? person.profile_path : "/assets/noImage.png"
       }`}
     />
+
+    </>
   );
 };
 
