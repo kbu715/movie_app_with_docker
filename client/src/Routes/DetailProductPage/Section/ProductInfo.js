d068c6eca661c5e25c71609df4ce5ea645db91dd
jiyoung <jiyoung4061@naver.com>
2020-08-11 13:45:08 +0900

    0811 jiyoung css

--- client/src/Routes/DetailProductPage/Section/ProductInfo.js
+++ client/src/Routes/DetailProductPage/Section/ProductInfo.js
@@ -12,15 +12,40 @@ function ProductInfo(props) {
     dispatch(addToCart(props.detail._id));
   };
   return (
-    <div>
-      <Descriptions title="상품정보" bordered>
-        <Descriptions.Item label="가격">{props.detail.price}</Descriptions.Item>
-        <Descriptions.Item label="Sold">{props.detail.sold}</Descriptions.Item>
-        <Descriptions.Item label="View">{props.detail.views}</Descriptions.Item>
-        <Descriptions.Item label="Description">
+    <div style={{ height:"50%", marginLeft:"5%", marginTop:"10%"}}>
+      {/* <Descriptions title="상품정보" bordered style={{border:"1px solid red", backgroundColor:"#f7f7f7"}}>
+        <Descriptions.Item label="가격" ><span style={{color:"#2e2e2e", }}>{props.detail.price}</span></Descriptions.Item>
+        <Descriptions.Item label="Sold"><span style={{color:"#2e2e2e", }}>{props.detail.sold}</span></Descriptions.Item>
+        <Descriptions.Item label="View"><span style={{color:"#2e2e2e", }}>{props.detail.views}</span></Descriptions.Item>
+        <Descriptions.Item label="Description"><span style={{color:"#2e2e2e"}}>
           {props.detail.description}
+        </span>
         </Descriptions.Item>
-      </Descriptions>
+      </Descriptions> */}
+      <table style={{ borderCollapse: "collapse", width:"100%", height:"100%", borderTop:"1px solid white" }}>
+            <tbody>
+              <tr style={{}}>
+                <th style={{ color: "#f7f7f7", fontSize:"20px", width:"50%", verticalAlign:"middle",borderBottom:"1px solid white" }}>가격</th>
+                <td style={{ verticalAlign:"middle", fontSize:"20px", textAlign:"center",borderBottom:"1px solid white" }}>
+                {props.detail.price}
+                </td>
+              </tr>
+              <tr>
+                <th style={{ color: "#f7f7f7", fontSize:"20px", width:"50%", verticalAlign:"middle",borderBottom:"1px solid white" }}>Sold</th>
+                <td style={{ verticalAlign:"middle", fontSize:"20px", textAlign:"center",borderBottom:"1px solid white" }}>{props.detail.sold}</td>
+              </tr>
+              <tr>
+                <th style={{ color: "#f7f7f7", fontSize:"20px", width:"50%", verticalAlign:"middle",borderBottom:"1px solid white" }}>View</th>
+                <td style={{ verticalAlign:"middle", fontSize:"20px", textAlign:"center",borderBottom:"1px solid white" }}>{props.detail.views}</td>
+              </tr>
+              <tr>
+                <th style={{ color: "#f7f7f7", fontSize:"20px", width:"50%", verticalAlign:"middle",borderBottom:"1px solid white" }}>Description</th>
+                <td style={{ verticalAlign:"middle", fontSize:"20px", textAlign:"center",borderBottom:"1px solid white" }}>
+                {props.detail.description}
+                </td>
+              </tr>
+            </tbody>
+          </table>
 
       <br />
       <br />
