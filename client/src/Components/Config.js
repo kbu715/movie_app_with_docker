65b6e21c36668c12ce8b2e7dbaf6017b7e0c8311
jinwoo <jintakk6531@gmail.com>
2020-08-20 16:10:42 +0900

    travis yml added

--- client/src/Components/Config.js
+++ client/src/Components/Config.js
@@ -11,5 +11,6 @@ export const LOCAL_SERVER = "http://localhost:5000/";
 export const CLIENT = "http://localhost:3000/";
 export const GOOGLE_KEY =
   "929257267887-jabje0s2v9gdvfrm1avh5qr1q63j9p91.apps.googleusercontent.com";
-export const GOOGLE_LOGO = "https://img0.yna.co.kr/etc/inner/KR/2015/09/02/AKR20150902004600091_07_i_P4.jpg";
+export const GOOGLE_LOGO =
+  "https://img0.yna.co.kr/etc/inner/KR/2015/09/02/AKR20150902004600091_07_i_P4.jpg";
 export const KAKAO_KEY = "4835800b3d8f6f4b36c85d157690121c";
