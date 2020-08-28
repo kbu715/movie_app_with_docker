30cf15627862368daf793a0f51bda6b76c25771e
장재영 <jangjaeyeong@MacBook-Pro-2.local>
2020-08-28 17:52:14 +0900

    재영&바울

--- client/src/Routes/Product/Product.js
+++ client/src/Routes/Product/Product.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Col, Card, Row } from "antd";
-import Meta from "antd/lib/card/Meta";
-import ImageSlider from "../../utils/ImageSlider";
-import SearchFeature from "./Section/SearchFeature";
-import styled from "styled-components";
-import { Helmet } from "react-helmet";
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import { Col, Card, Row } from "antd"
+import Meta from "antd/lib/card/Meta"
+import ImageSlider from "../../utils/ImageSlider"
+import SearchFeature from "./Section/SearchFeature"
+import styled from "styled-components"
+import { Helmet } from "react-helmet"
 
 const Button1 = styled.button`
   color: #9c88ff;
@@ -17,54 +17,66 @@ const Button1 = styled.button`
   margin-left: 20px;
   padding: 5px;
   box-shadow: 0 1.5rem 2rem rgba(156, 136, 255, 0.2);
-`;
+`
 
 function Product() {
-  const [Products, setProducts] = useState([]);
-  const [Skip, setSkip] = useState(0);
-  const [PostSize, setPostSize] = useState(0);
-  const [SearchTerm, setSearchTerm] = useState("");
-  const [Limit, setLimit] = useState(8);
+  const [Products, setProducts] = useState([])
+  const [Skip, setSkip] = useState(0)
+  const [PostSize, setPostSize] = useState(0)
+  const [SearchTerm, setSearchTerm] = useState("")
+  const [Limit, setLimit] = useState(8)
 
   useEffect(() => {
-    let body = {
-      skip: Skip,
-      limit: Limit,
-    };
+    // let body = {
+    //   skip: Skip,
+    //   limit: Limit,
+    // };
 
-    getProducts(body);
+    getProducts();
   }, []);
 
   const getProducts = (body) => {
     axios.post("/api/product/products", body).then((response) => {
-      console.log("22222", response.data);
+      // console.log("22222", response.data)
       if (response.data.success) {
-        if (body.loadMore) {
-          setProducts([...Products, ...response.data.productInfo]);
-        } else {
-          setProducts(response.data.productInfo);
-        }
-        setPostSize(response.data.postSize);
+        // if (body.loadMore) {
+        //   setProducts([...Products, ...response.data.productInfo])
+        // } else {
+          setProducts(response.data.productInfo)
+        // }
+        // setPostSize(response.data.postSize)
       } else {
-        alert("상품들을 가져오는데 실패 했습니다.");
+        alert("상품들을 가져오는데 실패 했습니다.")
       }
-    });
-  };
+    })
+  }
 
-  const loadMoreHandler = () => {
-    let skip = Skip + Limit;
+  // const loadMoreHandler = () => {
+  //   let skip = Skip + Limit;
 
-    let body = {
-      skip: skip,
-      limit: Limit,
-      loadMore: true,
-    };
+  //   let body = {
+  //     skip: skip,
+  //     limit: Limit,
+  //     loadMore: true,
+  //   };
 
-    getProducts(body);
-    setSkip(skip);
-  };
+  //   getProducts(body);
+  //   setSkip(skip);
+  // };
 
-  const renderCards = Products.map((product, index) => {
+  const renderCards = Products.sort((a, b) => {
+    let aContinents = a.continents
+    let bContinents = b.continents
+
+    if (aContinents < bContinents) {
+      return 1
+    } else if (aContinents > bContinents) {
+      return -1
+    } else {
+      return 0
+    }
+  }).map((product, index) => {
+    // console.log("dddd", product)
     return (
       <Col lg={6} md={8} xs={20} key={index}>
         <a href={`/product/${product._id}`}>
@@ -85,7 +97,9 @@ function Product() {
           style={{
             position: "relative",
             textAlign: "center",
-            fontSize: "10px",
+            fontSize: "12px",
+            marginTop: "10px",
+            color: "#999999",
           }}
         >
           {product.description}
@@ -102,19 +116,19 @@ function Product() {
           {product.price}원
         </div>
       </Col>
-    );
-  });
+    )
+  })
 
   const updateSearchTerm = (newSearchTerm) => {
     let body = {
-      skip: 0,
-      limit: Limit,
+      // skip: 0,
+      // limit: Limit,
       searchTerm: newSearchTerm,
-    };
-    setSkip(0);
-    setSearchTerm(newSearchTerm);
-    getProducts(body);
-  };
+    }
+    // setSkip(0)
+    setSearchTerm(newSearchTerm)
+    getProducts(body)
+  }
 
   return (
     <>
@@ -145,14 +159,15 @@ function Product() {
               width: "50%",
             }}
           >
-            <SearchFeature refreshFunction={updateSearchTerm} />
+            
+            <SearchFeature refreshFunction={updateSearchTerm}/>
           </div>
         </div>
         <br />
         <br />
         <Row gutter={(16, 16)}>{renderCards}</Row>
         <br />
-        {PostSize >= Limit && (
+        {/* {PostSize >= Limit && (
           <div
             style={{
               display: "flex",
@@ -165,13 +180,13 @@ function Product() {
               color="primary"
               onClick={loadMoreHandler}
             >
-              더보기
+              기
             </Button1>
           </div>
-        )}
+        )} */}
       </div>
     </>
-  );
+  )
 }
 
-export default Product;
+export default Product
